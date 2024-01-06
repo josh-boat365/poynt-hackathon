8ee9b3b347488664987f11c4d3adc966265be2ce
orekode <shalommckdavid@gmail.com>
2024-01-06 00:16:12 +0000

    changes made to products response - include category

--- database/migrations/2024_01_04_140105_create_orders_table.php
+++ database/migrations/2024_01_04_140105_create_orders_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Str;
 
 return new class extends Migration
 {
@@ -18,6 +17,7 @@ return new class extends Migration
             $table->string('status')->default('pending');
             $table->string('reference')->default(uniqid());
             $table->timestamps();
+            
         });
     }
 
